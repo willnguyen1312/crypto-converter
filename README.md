ccc4e5d17bdfb463a92fd7fbe08aa249178bab41
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-16 14:07:51 -0300

    Implement Crypto converter solution 🚀

---
+++ README.md
@@ -0,0 +1,41 @@
+# [Crypto Converter](https://frontendeval.com/questions/crypto-converter)
+
+## Solution
+
+### Constants
+
+- `API_ENDPOINT` - https://api.frontendeval.com/fake/crypto/
+- `REFRESH_TIME` - 10000 milliseconds (10 seconds)
+
+### State
+
+- amount: `number` - The amount of the currency to convert.
+- currency: `string` - The currency to convert from.
+- lastValue: `number` - The last computed value of the conversion.
+- currentValue: `number` - The current computed value of the conversion.
+
+### Rendering
+
+Simple form with two inputs of amount and currency. The amount input is a number input and the currency input is a select input with the options of the currencies
+
+The result is displayed when there are valid values in both inputs
+
+The changed value is displayed when there are enough information about last and current computed values to calculate the difference
+
+Everything is rendered in the center horizontally by using flexbox
+
+### Crypto conversion logic
+
+- `fetchValue`: The conversion is done by using the `fetch` API to get the conversion rate from the API endpoint. Then update the last value and current value in the state.
+
+- `intervalId`: The interval is set up by using `setInterval` to call `fetchValue` every `REFRESH_TIME` milliseconds. This will be cleaned on up unmount lifecycle to avoid memory leak.
+
+- There is a single watcher to watch the changes of `amount` and `currency` in the state. When there are changes in either of them, lastValue and currentValue will be reset and the `fetchValue` will be called to fetch the new conversion rate. The current interval will be cleared if available and a new interval will be set up.
+
+- There are two computed values `changedValue` and `displayChangedValue` to avoid clustering the template with logic. `changedValue` is the difference between lastValue and currentValue. `displayChangedValue` is the formatted value of `changedValue` with the up / down symbols. The `changedValue` is also used to determine the color of the result.
+
+### Nice to have
+
+- [x] Add a loading indicator
+- [x] Add a refresh button
+- [x] Add a error message when the API call fails
---
+++ src/App.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import { computed, onUnmounted, ref, watch } from "vue";
+
+const API_ENDPOINT = "https://api.frontendeval.com/fake/crypto/";
+type JSONResponse = { value: number };
+const REFRESH_TIME = 10000; // 10 seconds
+
+const currencies = ["USD", "EUR", "GBP", "CNY", "JPY"] as const;
+type Currency = (typeof currencies)[number];
+
+const lastValue = ref<number>();
+const currentValue = ref<number>();
+const amount = ref<number>();
+const currency = ref<Currency>(currencies[0]);
+let intervalId: number | null = null;
+
+const getRoundedValue = (value: number) => Math.round(value * 100) / 100;
+
+const fetchValue = async (amount: number, currency: Currency) => {
+  if (!amount || !currency) return;
+
+  const response = await fetch(`${API_ENDPOINT}${currency}`);
+  if (response.ok) {
+    const data = (await response.json()) as JSONResponse;
+
+    lastValue.value = currentValue.value;
+    currentValue.value = getRoundedValue(data.value * amount);
+    return;
+  }
+
+  // TODO: error handling
+};
+
+const changedValue = computed(() => {
+  if (lastValue.value && currentValue.value) {
+    return getRoundedValue(currentValue.value - lastValue.value);
+  }
+
+  return 0;
+});
+
+const displayChangedValue = computed(() => {
+  if (changedValue.value) {
+    return changedValue.value > 0
+      ? `↑ ${changedValue.value}`
+      : `↓ ${changedValue.value}`;
+  }
+
+  return "";
+});
+
+watch([amount, currency, amount], async ([newAmount, newCurrency]) => {
+  lastValue.value = undefined;
+  currentValue.value = undefined;
+
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+
+  if (newAmount && newAmount > 0) {
+    await fetchValue(newAmount, newCurrency);
+    intervalId = setInterval(() => {
+      amount.value && fetchValue(amount.value, currency.value);
+    }, REFRESH_TIME);
+  }
+});
+
+onUnmounted(() => {
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+});
+</script>
+
+<template>
+  <form class="inputForm">
+    <input v-model="amount" />
+
+    <select v-model="currency">
+      <option v-for="currency in currencies" :value="currency">
+        {{ currency }}
+      </option>
+    </select>
+  </form>
+
+  <div class="result" v-if="currentValue">
+    <p>{{ currentValue }} WUC</p>
+    <p
+      v-if="changedValue"
+      :class="{
+        valueUp: changedValue > 0,
+        valueDown: changedValue < 0,
+      }"
+    >
+      {{ displayChangedValue }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.inputForm {
+  display: flex;
+  justify-content: center;
+  gap: 4px;
+}
+
+.result {
+  display: flex;
+  justify-content: center;
+  gap: 16px;
+}
+
+.valueUp {
+  color: green;
+}
+
+.valueDown {
+  color: red;
+}
+</style>
